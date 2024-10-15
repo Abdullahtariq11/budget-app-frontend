8aad4ef683fd36692122819306511430f9a42a66
Abdullah Tariq <abdullahtariq096@gmail.com>
2024-10-14 20:18:05 -0700

    Login integration with backend Implemented

--- src/Components/LoginPage/LoginForm.jsx
+++ src/Components/LoginPage/LoginForm.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.css";
+import axios from "axios";
 
 function LoginForm({setTab}) {
+    const [username,setUsername]=useState("");
+    const [password,setPassword]=useState("");
+    const [Error,setError]=useState("");
+
+    const handleLogin= async(e)=>{
+        e.preventDefault();
+
+        try 
+        {
+            const response = await axios.post("http://localhost:5115/api/Users/Login",{
+                username,
+                password
+            });
+            const {token}=response.data;
+            localStorage.setItem("authToken",token)
+            
+        } 
+        catch (error) {
+            setError(error.response.data.Message);
+            
+        }
+    }
+
   return (
     <div className="login-form-container">
       <h2>Log In</h2>
-      <form className="login-form">
-        <input type="text" placeholder="username" required />
-        <input type="text" placeholder="Password" required />
-        <button>Login</button>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input type="text" placeholder="username" onChange={(e)=>setUsername(e.target.value)} value={username} required />
+        <input type="text" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} value={password} required />
+        <button type="submit">Login</button>
         <p>Forgot Password?</p>
         <p>
           Don't have an account? <span className="switch-tab" onClick={()=>setTab("Signup")}> Signup</span>
